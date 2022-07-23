b665124aaafce070782ea729c4aebb49f146177d
Juan Antonio Soto C <renitope@gmail.com>
2022-07-23 18:47:55 -0500

    Tipado typescript

---
+++ src/bases/01-types.ts
@@ -0,0 +1,11 @@
+export let name: string = "Antonio";
+export const age: number = 35;
+export const isValid: boolean = true;
+
+name = "melissa";
+
+export const templateString = `
+nombre: ${name},
+edad: ${age},
+booleano: ${isValid}
+`;
